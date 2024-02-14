46a5dc94bae51ddc28897ac0ad79b6acaaa270f7
albertomaffeii <albertomaffeii@gmail.com>
2024-02-14 15:44:47 +0100

    v1.0.2: request.rest, create services

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,7 +1,53 @@
 const { Service: ServiceModel } = require("../models/Service");
 
 const serviceController = {
-    
+    create: async (req, res) => {
+        try {
+            const service = {
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
+            };
+
+            const response = await ServiceModel.create(service);
+
+            res.status(201).json({
+                response,
+                msg: "Service created successfully!",
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    show: async (req, res) => {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    store: async (req, res) => {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 
 module.exports = serviceController;
